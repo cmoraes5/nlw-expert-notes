4ea036baf9ec92898859171357d009a3471c531c
Caio Moraes V Santos <caio.moraes@radixeng.com>
2024-02-07 09:06:34 -0300

    ✨ feat: adding modal to aplication

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/New-note-card'
 import { NoteCard } from './components/Note-card'
 
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -21,7 +22,10 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }} />
 
       </div>
     </div>
